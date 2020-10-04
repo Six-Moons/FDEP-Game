c2033a4b3af397e5db7479955492d8fa215c5745
Humberto González Sánchez <hglez.98s@gmail.com>
2020-10-04 11:03:46 -0500

    Correccion de generacion aleatoria

--- Basurita/trash.gd
+++ Basurita/trash.gd
@@ -8,9 +8,8 @@ var is_open := false
 func interaction_can_interact(interactionComponentParent : Node) -> bool:
 	return interactionComponentParent is Player
 
-# Not implemented - we'll use the default texture instead
-#func interaction_get_texture() -> Texture:
-#	pass
+func _ready():
+	rng.randomize()
 
 func interaction_get_text() -> String:
 	return "Buscar"
