65ece59a11f138be3c5cc8d1d4ff9aaf0f43a1d9
Humberto González Sánchez <hglez.98s@gmail.com>
2020-10-04 10:47:55 -0500

    Inventario, obtencion de objetos

---
+++ level.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+var hats = ["Vaquero", "Marinero", "Pirata", "DDD"]
+var shirts = ["Vaquero", "Marinero", "Pirata", "DDD"]
+var accesories = ["Vaquero", "Marinero", "Pirata", "DDD"]
+var hasHat = false
+var hasShirt = false
+var hasAcc = false
+var owned = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
