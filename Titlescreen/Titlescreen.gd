c578aeda31fa8d3b780dc0f76562da2a9ba13f24
albgv1402 <albertogarv1402@gmail.com>
2020-10-04 07:55:21 -0500

    Pantalla de inicio animada

---
+++ Titlescreen/Titlescreen.gd
@@ -0,0 +1,27 @@
+extends CanvasLayer
+
+onready var tween_values = [1, 1.2]
+onready var initialScale = $StartButton.scale
+
+func _ready():
+	_start_tween()
+
+func _start_tween():
+	$StartButton.scale = initialScale
+	$StartButton/Tween.interpolate_property($StartButton, 
+								'scale', 
+								$StartButton.scale * tween_values[0], 
+								$StartButton.scale * tween_values[1], 
+								1, 
+								Tween.TRANS_QUAD,
+								Tween.EASE_IN_OUT)  
+	$StartButton/Tween.start()
+
+
+func _on_Tween_tween_completed(object, key):
+#	var n = tween_values[0]
+#	tween_values[0] = tween_values[1]
+#	tween_values[1] = n
+	tween_values.invert()
+	_start_tween()
+	pass # Replace with function body.
