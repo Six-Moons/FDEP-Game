53bbd1c79602941b23c6d8520dd1b3095e27c6cf
albgv1402 <albertogarv1402@gmail.com>
2020-10-08 19:52:27 -0500

    Pantalla de inicio terminada

---
+++ Titlescreen/Credits.gd
@@ -0,0 +1,6 @@
+extends Control
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Titlescreen/Titlescreen.tscn")
+#	print("peepee")
+	pass
--- Titlescreen/Titlescreen.gd
+++ Titlescreen/Titlescreen.gd
@@ -1,32 +1,42 @@
-extends CanvasLayer
+extends Control
 
 var changingScene = false
 onready var tween_values = [1, 1.2]
-onready var initialScale = $StartButton.scale
+#onready var initialScale = $StartButton.scale
 
 func _ready():
 #	_start_tween()
 	pass
 
-func _start_tween():
-	$StartButton.scale = initialScale
-	$StartButton/Tween.interpolate_property($StartButton, 
-								'scale', 
-								$StartButton.scale * tween_values[0], 
-								$StartButton.scale * tween_values[1], 
-								1, 
-								Tween.TRANS_QUAD,
-								Tween.EASE_IN_OUT)  
-	$StartButton/Tween.start()
+#func _start_tween():
+#	$StartButton.scale = initialScale
+#	$StartButton/Tween.interpolate_property($StartButton, 
+#								'scale', 
+#								$StartButton.scale * tween_values[0], 
+#								$StartButton.scale * tween_values[1], 
+#								1, 
+#								Tween.TRANS_QUAD,
+#								Tween.EASE_IN_OUT)  
+#	$StartButton/Tween.start()
+#
+#func _input(event):
+#	if (Input.is_action_pressed("ui_accept") and !changingScene):
+#		changingScene = true
+#		SceneChanger.change_scene("res://Levels/village.tscn")
 
-func _input(event):
-	if (Input.is_action_pressed("ui_accept") and !changingScene):
-		changingScene = true
-		SceneChanger.change_scene("res://Levels/village.tscn")
+
+#
+#func _on_Tween_tween_completed(object, key):
+#	tween_values.invert()
+#	_start_tween()
+#	pass # Replace with function body.
 
 
+func _on_PlayButton_pressed():
+	get_tree().change_scene($Menu/CenterRow/Buttons/PlayButton.scene_to_load)
+	pass # Replace with function body.
 
-func _on_Tween_tween_completed(object, key):
-	tween_values.invert()
-	_start_tween()
+
+func _on_CreditsButton_pressed():
+	get_tree().change_scene($Menu/CenterRow/Buttons/CreditsButton.scene_to_load)	
 	pass # Replace with function body.
