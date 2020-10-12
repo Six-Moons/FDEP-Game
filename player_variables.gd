32a4fcc8ba98ca1b3198e504c3210df3889c7584
albgv1402 <albertogarv1402@gmail.com>
2020-10-12 12:34:05 -0500

    Reimporte pantalla de inicio y actualice el path para las capturas de pantalla

--- player_variables.gd
+++ player_variables.gd
@@ -8,12 +8,16 @@ var hasShirt
 var hasAcc
 var owned
 var ss = 0
+var rng = RandomNumberGenerator.new()
 
 func takeScreenshot():
 	var img = get_viewport().get_texture().get_data()
 	# Flip on the Y axis.
 	# You can also set "V Flip" to true if not on the root Viewport.
 	img.flip_y()
-	img.save_png("res://Screenshots/screenshot" + str(ss) + ".png")
+	
+	rng.randomize()
+	var randNum = int(round(rng.randf_range(0.0, 2.0)))
+	img.save_png("res://Sprites/ignore/Screenshots/screenshot" + str(randNum) + "_" + str(ss) + ".png")
 	ss += 1
 	pass
