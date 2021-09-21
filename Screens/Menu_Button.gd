6dd51f1a47c92d2abfb365d16a107241330b4071
Ale García <alegayndra@gmail.com>
2021-09-21 17:30:06 -0500

    Arreglé creditos no funcionando

--- Screens/Menu_Button.gd
+++ Screens/Menu_Button.gd
@@ -2,12 +2,10 @@ extends Button
 
 export (String) var scene_to_load
 
-
 func _on_Menu_Button_mouse_entered():
 	$ColorRect.hide()
 	$ColorRect2.show()
 	pass # Replace with function body.
-
 
 func _on_Menu_Button_mouse_exited():
 	$ColorRect.show()
